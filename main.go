4fe1f99e094b9f3ffeeada52c39220cfcd8cf752
Ultimace1314 <assassinoffaith@gmail.com>
2025-03-06 08:55:44 -0500

    reads/writes to json config file

--- main.go
+++ main.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/Ultimace1314/blogAggregator/internal/config"
+)
+
 func main() {
-	// readConfig()
-	config.readConfig()
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("nate")
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
 }
