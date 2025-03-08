6a8c7dbc9ae4a797d619b30446facaf4594d27d5
Ultimace1314 <assassinoffaith@gmail.com>
2025-03-07 19:01:15 -0500

    created reset command to delete users in user table

--- handler_user.go
+++ handler_user.go
@@ -66,3 +66,17 @@ func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.Name)
 }
+
+func handlerResetUsers(s *state, cmd command) error {
+	if len(cmd.Args) != 0 {
+		return fmt.Errorf("usage: %s", cmd.Name)
+	}
+
+	err := s.db.ResetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't reset users: %w", err)
+	}
+
+	fmt.Println("Users table reset successfully!")
+	return nil
+}
